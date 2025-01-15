60b30a6aafa2c23853b7a50df45d67ba87e919fb
Niz <mn@furzoom.com>
2025-01-15 20:49:19 +0800

    Improve error message
    Signed-off-by: Niz <mn@furzoom.com>

--- main.c
+++ main.c
@@ -21,6 +21,9 @@ struct Token {
   int len;          // token length
 };
 
+// Input string.
+static char* current_input;
+
 // Reports an error and exit.
 static void error(char* fmt, ...) {
   va_list ap;
@@ -31,6 +34,32 @@ static void error(char* fmt, ...) {
   exit(1);
 }
 
+// Reports an error localtion and exit.
+static void verror_at(char* loc, char* fmt, va_list ap) {
+  int pos = loc - current_input;
+  fprintf(stderr, "%s\n", current_input);
+  fprintf(stderr, "%*s", pos, "");  // print pos spaces
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+
+static void error_at(char* loc, char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(loc, fmt, ap);
+  va_end(ap);
+}
+
+static void error_tok(Token* tok, char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(tok->loc, fmt, ap);
+  va_end(ap);
+}
+
 // Consumes the current token if it matches `op`.
 static bool equal(Token* tok, char* op) {
   return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
@@ -39,7 +68,7 @@ static bool equal(Token* tok, char* op) {
 // Ensure that the current token is 's'.
 static Token* skip(Token* tok, char* s) {
   if (!equal(tok, s)) {
-    error("expected '%s'", s);
+    error_tok(tok, "expected '%s'", s);
   }
   return tok->next;
 }
@@ -47,7 +76,7 @@ static Token* skip(Token* tok, char* s) {
 // Ensure that the current token is TK_NUM.
 static int get_number(Token* tok) {
   if (tok->kind != TK_NUM) {
-    error("expected a number");
+    error_tok(tok, "expected a number");
   }
   return tok->val;
 }
@@ -62,7 +91,8 @@ static Token* new_token(TokenKind kind, char* start, char* end) {
 }
 
 // Tokenize `p` and returns new tokens.
-static Token* tokenize(char* p) {
+static Token* tokenize() {
+  char* p = current_input;
   Token head = {};
   Token* cur = &head;
 
@@ -88,7 +118,7 @@ static Token* tokenize(char* p) {
       continue;
     }
 
-    error("invalid token");
+    error_at(p, "invalid token");
   }
 
   cur->next = new_token(TK_EOF, p, p);
@@ -97,11 +127,11 @@ static Token* tokenize(char* p) {
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
-    return 1;
+    error("%s: invalid number of arguments\n", argv[0]);
   }
 
-  Token* tok = tokenize(argv[1]);
+  current_input = argv[1];
+  Token* tok = tokenize();
 
   printf("  .globl main\n");
   printf("main:\n");

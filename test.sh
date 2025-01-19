061c19a0a4ca82c0f8db4d9bb0baaccfd86f53d6
Niz <mn@furzoom.com>
2025-01-19 18:55:38 +0800

    Accept multiple statements separated by semicolons
    Signed-off-by: Niz <mn@furzoom.com>

--- test.sh
+++ test.sh
@@ -16,38 +16,40 @@ assert() {
   fi
 }
 
-assert 0 0
-assert 2 2
-assert 100 100
-assert 6 '4+5-3'
-assert 41 '12 + 34 - 5'
-assert 47 '5+6*7'
-assert 15 '5*(9-6)'
-assert 4 '(3+5)/2'
-assert 10 '-10+20'
-assert 10 '- -10'
-assert 10 '- - +10'
-assert 80 '8 * - - 10'
+assert 0 '0;'
+assert 2 '2;'
+assert 100 '100;'
+assert 6 '4+5-3;'
+assert 41 '12 + 34 - 5;'
+assert 47 '5+6*7;'
+assert 15 '5*(9-6);'
+assert 4 '(3+5)/2;'
+assert 10 '-10+20;'
+assert 10 '- -10;'
+assert 10 '- - +10;'
+assert 80 '8 * - - 10;'
 
 #
-assert 0 '0==1'
-assert 1 '13==13'
-assert 1 '0!=1'
-assert 0 '14!=14'
+assert 0 '0==1;'
+assert 1 '13==13;'
+assert 1 '0!=1;'
+assert 0 '14!=14;'
 
-assert 1 '0<1'
-assert 0 '1<1'
-assert 0 '2<1'
-assert 1 '0<=1'
-assert 1 '1<=1'
-assert 0 '2<=1'
+assert 1 '0<1;'
+assert 0 '1<1;'
+assert 0 '2<1;'
+assert 1 '0<=1;'
+assert 1 '1<=1;'
+assert 0 '2<=1;'
 
-assert 1 '1>0'
-assert 0 '1>1'
-assert 0 '1>2'
-assert 1 '1>=0'
-assert 1 '1>=1'
-assert 0 '1>=2'
+assert 1 '1>0;'
+assert 0 '1>1;'
+assert 0 '1>2;'
+assert 1 '1>=0;'
+assert 1 '1>=1;'
+assert 0 '1>=2;'
+
+assert 3 '1;2;3;'
 
 echo OK
 

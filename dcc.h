2e17d2ca9e8212bbcfae428be2550df0e6f98585
Niz <mn@furzoom.com>
2025-02-26 21:54:20 +0800

    Add keyword "int" and make variable definiion mandatory
    Signed-off-by: Niz <mn@furzoom.com>

--- dcc.h
+++ dcc.h
@@ -40,6 +40,7 @@ void error_at(char* loc, char* fmt, ...);
 void error_tok(Token* tok, char* fmt, ...);
 bool equal(Token* tok, char* op);
 Token* skip(Token* tok, char* op);
+bool consume(Token** rest, Token* tok, char* str);
 Token* tokenize(char* input);
 
 //
@@ -50,8 +51,9 @@ Token* tokenize(char* input);
 typedef struct Obj Obj;
 struct Obj {
   Obj* next;
-  char* name;  // Variable name
-  int offset;  // Offset from RBP
+  char* name;   // Variable name
+  Type* ty;     // Type
+  int offset;   // Offset from RBP
 };
 
 // Function
@@ -122,12 +124,18 @@ typedef enum {
 
 struct Type {
   TypeKind kind;
+
+  // Pointer
   Type* base;
+
+  // Declaration
+  Token* name;
 };
 
 extern Type* ty_int;
 
 bool is_integer(Type* ty);
+Type* pointer_to(Type* base);
 void add_type(Node* node);
 
 //
--- tokenize.c
+++ tokenize.c
@@ -51,6 +51,16 @@ Token* skip(Token* tok, char* s) {
   return tok->next;
 }
 
+bool consume(Token** rest, Token* tok, char* str) {
+  if (equal(tok, str)) {
+    *rest = tok->next;
+    return true;
+  }
+
+  *rest = tok;
+  return false;
+}
+
 // Create a new token.
 static Token* new_token(TokenKind kind, char* start, char* end) {
   Token* tok = calloc(1, sizeof(Token));
@@ -91,6 +101,7 @@ static bool is_keyword(Token* tok) {
     "else",
     "for",
     "while",
+    "int",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {

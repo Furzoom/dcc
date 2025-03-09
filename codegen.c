1daa6e14113236a1cb70c5b327c174bf09b0dd21
Niz <mn@furzoom.com>
2025-03-09 21:29:24 +0800

    Add global variables
    Signed-off-by: Niz <mn@furzoom.com>

--- codegen.c
+++ codegen.c
@@ -53,7 +53,13 @@ static void store(void) {
 static void gen_addr(Node* node) {
   switch(node->kind) {
     case ND_VAR:
-      printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+      if (node->var->is_local) {
+        // Local variable
+        printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+      } else {
+        // Global variable
+        printf("  lea %s(%%rip), %%rax\n", node->var->name);
+      }
       return;
     case ND_DEREF:
       gen_expr(node->lhs);
@@ -216,7 +222,20 @@ static void assign_lvar_offsets(Obj* prog) {
   }
 }
 
-void codegen(Obj* prog) {
+static void emit_data(Obj* prog) {
+  for (Obj* var = prog; var; var = var->next) {
+    if (var->is_function) {
+      continue;
+    }
+
+    printf("  .data\n");
+    printf("  .globl %s\n", var->name);
+    printf("%s:\n", var->name);
+    printf("  .zero %d\n", var->ty->size);
+  }
+}
+
+static void emit_text(Obj* prog) {
   assign_lvar_offsets(prog);
 
   for (Obj* fn = prog; fn; fn = fn->next) {
@@ -250,5 +269,11 @@ void codegen(Obj* prog) {
     printf("  pop %%rbp\n");
     printf("  ret\n");
   }
+}
+
+void codegen(Obj* prog) {
+  assign_lvar_offsets(prog);
+  emit_data(prog);
+  emit_text(prog);
 }
 
